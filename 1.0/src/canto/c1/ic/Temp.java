cefd57d706fde701edf90eda9042a930b7f66f02
Xudong Zheng <dong128@gmail.com>
2010-05-29 11:26:59 +0000

    补充Temp的成员变量和方法

--- 1.0/src/canto/c1/ic/Temp.java
+++ 1.0/src/canto/c1/ic/Temp.java
@@ -4,7 +4,15 @@ package canto.c1.ic;
  * 中间代码的临时变量操作数
  */
 public class Temp extends Location {
-
+	
+	private static int count = 0;
+	
+	private final int id;
+	
+	public Temp() {
+		this.id = ++count;
+	}
+	
 	@Override
 	public void accept(ICVisitor visitor) throws Exception {
 		visitor.visit(this);
@@ -13,6 +21,18 @@ public class Temp extends Location {
 	@Override
 	public int getICType() {
 		return TEMP;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (!(obj instanceof Temp)) return false;
+		if (id != ((Temp) obj).getID()) return false;
+		return true;
+	}
+	
+	public int getID() {
+		return id;
 	}
 
 }
