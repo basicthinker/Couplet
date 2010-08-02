14e819af831486e9afa6493305266acb39422b69
Yaning Liu <liugoodness@gmail.com>
2010-08-02 12:28:42 +0000

    添加了TCGenerator

--- 1.0/src/canto/c1/TCGenerator.java
+++ 1.0/src/canto/c1/TCGenerator.java
@@ -2,9 +2,45 @@ package canto.c1;
 
 import canto.IntermediateCode;
 import canto.TargetCode;
+import canto.c1.ic.Add;
+import canto.c1.ic.Goto;
+import canto.c1.ic.ICVisitor;
+import canto.c1.ic.In;
+import canto.c1.ic.InstructionList;
+import canto.c1.ic.IntegerLiteral;
+import canto.c1.ic.JEQ;
+import canto.c1.ic.JGE;
+import canto.c1.ic.JGT;
+import canto.c1.ic.JLE;
+import canto.c1.ic.JLT;
+import canto.c1.ic.JNE;
+import canto.c1.ic.Label;
+import canto.c1.ic.Location;
+import canto.c1.ic.Mov;
+import canto.c1.ic.Mul;
+import canto.c1.ic.Neg;
+import canto.c1.ic.Out;
+import canto.c1.ic.Sub;
+import canto.c1.ic.Temp;
+import canto.c1.ic.Variable;
 
-public class TCGenerator implements canto.TCGenerator {
+public class TCGenerator implements canto.TCGenerator,ICVisitor  {
 
+
+	/**输入的中间代码序列*/
+	private canto.IntermediateCode ic;
+	
+	/**产生的代码序列*/
+	private InstructionList instructionList;
+	
+	/**C1的符号表，此处是个名值对照表*/
+	private SymbolTable<Location> symbolTable;
+
+	public ICGenerator(){
+		instructionList=new InstructionList();
+		symbolTable=new SymbolTable<Location>();
+	}
+	
 	@Override
 	public void setIC(IntermediateCode ic) {
 		// TODO Auto-generated method stub
@@ -21,6 +57,120 @@ public class TCGenerator implements canto.TCGenerator {
 	public TargetCode getTC() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void visit(InstructionList ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Label ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(In ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Out ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Goto ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JEQ ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JNE ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JLT ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JLE ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JGT ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(JGE ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Mov ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Add ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Sub ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Mul ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Neg ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Variable ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Temp ic) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(IntegerLiteral ic) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
